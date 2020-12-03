94f43c23907a6a8640ed6d801bff8d5381d2acdd
Amanda Ernar <amanda.medetgul.ernar@gmail.com>
2020-12-02 21:41:24 -0800

    test commit 3

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://github.com/aalchemize"
+          href="https://github.com/amandaernar"
           target="_blank"
           rel="noopener noreferrer"
         >
