43cf90f4a7a4e00e2c06d62b7dfa865d9730e2eb
Amanda Ernar <amanda.medetgul.ernar@gmail.com>
2020-12-04 16:05:51 -0800

    edited App.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, world!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://amandaernar.github.io/blog"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My blog
         </a>
       </header>
     </div>
