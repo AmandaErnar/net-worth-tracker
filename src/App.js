b8c6ec3392b1592c0684c45fc0424c7dfdb2022d
Amanda Ernar <amanda.medetgul.ernar@gmail.com>
2020-12-06 19:49:33 -0800

    made changes to App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Chicken
+          Hello World
         </p>
         <a
           className="App-link"
